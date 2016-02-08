ee82d122481b0517e6ea59cf36408dbcc31f7d5e
orizens <farhioren@gmail.com>
2016-02-08 12:11:45 +0200

    updated index.js

---
+++ angular2-infinite-scroll.ts
@@ -0,0 +1,9 @@
+import { InfiniteScroll } from './src/infinite-scroll';
+import { Scroller } from './src/scroller';
+
+export * from './src/infinite-scroll';
+export * from './src/scroller';
+
+export default {
+	directives: [ InfiniteScroll, Scroller ]
+}
--- index.js
+++
@@ -1,1 +0,0 @@
-require('./src/infinite-scroll');
