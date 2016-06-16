15cea1da3b530a882cbea08707922eb393dfaf98
orizens <farhioren@gmail.com>
2016-06-16 20:06:28 +0300

    fixed travis build issues, fixes proper tests with mocked ElementRef type

--- src/infinite-scroll.spec.ts
+++ src/infinite-scroll.spec.ts
@@ -9,15 +9,22 @@ import {
   beforeEachProviders
 } from '@angular/core/testing';
 import { InfiniteScroll } from './infinite-scroll'; 
+import { ElementRef } from '@angular/core'; 
+
 describe('Infinite Scroll Directive', () => {
+  const createMockElement = () => {
+    const mockedElement: ElementRef = new ElementRef(document.createElement('div'));
+    return mockedElement;
+  };
+  
   it('should create an instance of the directive', () => {
-    const mockedElement = {};
+    const mockedElement = createMockElement();
     const actual = new InfiniteScroll(mockedElement);
     expect(actual).toBeDefined();
   });
 
   it('should have default @Input properties values', () => {
-    const mockedElement: any = {};
+    const mockedElement = createMockElement();
     const directive = new InfiniteScroll(mockedElement);
     const expectedInputs = [
       '_distanceDown',
@@ -31,7 +38,7 @@ describe('Infinite Scroll Directive', () => {
   });
 
   it('should trigger the onScrollDown event when scroll has passed _distandDown', () => {
-    const mockedElement: any = {};
+    const mockedElement = createMockElement();
     const directive = new InfiniteScroll(mockedElement);
     spyOn(directive, 'onScrollDown');
     directive.ngOnInit();
@@ -44,7 +51,7 @@ describe('Infinite Scroll Directive', () => {
   });
 
   it('should trigger the onScrollUp event when scroll has passed _distanceUp', () => {
-    const mockedElement = {};
+    const mockedElement = createMockElement();
     const directive = new InfiniteScroll(mockedElement);
     spyOn(directive, 'onScrollUp');
     directive.ngOnInit();
