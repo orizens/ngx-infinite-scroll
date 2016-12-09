252ba10199f279366c00e69dfdddf36bd2e1adf1
Oren Farhi <farhioren@gmail.com>
2016-12-09 10:12:52 +0200

    attemp to resolve AOT issues

--- angular2-infinite-scroll.ts
+++ angular2-infinite-scroll.ts
@@ -3,11 +3,11 @@ import { Scroller } from './src/scroller';
 import { PositionResolver } from './src/position-resolver';
 import { AxisResolver } from './src/axis-resolver';
 
-export * from './src/infinite-scroll';
-export * from './src/scroller';
-export * from './src/position-resolver';
-export * from './src/axis-resolver';
-export * from './src/index';
+export { InfiniteScroll } from './src/infinite-scroll';
+export { Scroller, InfiniteScrollEvent } from './src/scroller';
+export { PositionResolver, PositionResolverFactory, PositionElements } from './src/position-resolver';
+export { AxisResolver, AxisResolverFactory } from './src/axis-resolver';
+export { InfiniteScrollModule } from './src/index';
 
 export default {
 	directives: [ InfiniteScroll, Scroller, AxisResolver, PositionResolver ]
