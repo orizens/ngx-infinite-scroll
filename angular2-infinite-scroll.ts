f377a3e46a5f872839076b432b1105836501647b
Oren Farhi <farhioren@gmail.com>
2017-01-23 16:31:07 +0200

    remove scroller refs

--- angular2-infinite-scroll.ts
+++ angular2-infinite-scroll.ts
@@ -1,12 +1,10 @@
 import { InfiniteScroll } from './src/infinite-scroll';
-import { Scroller } from './src/scroller';
 import { PositionResolver } from './src/position-resolver';
 import { AxisResolver } from './src/axis-resolver';
 import { ScrollRegister } from './src/scroll-register';
 import { ScrollResolver } from './src/scroll-resolver';
 
 export { InfiniteScroll } from './src/infinite-scroll';
-export { Scroller, InfiniteScrollEvent } from './src/scroller';
 export { PositionResolver, PositionResolverFactory } from './src/position-resolver';
 export { AxisResolver, AxisResolverFactory } from './src/axis-resolver';
 export { InfiniteScrollModule } from './src/index';
@@ -14,5 +12,5 @@ export { ScrollRegister } from './src/scroll-register';
 export { ScrollResolver } from './src/scroll-resolver';
 
 export default {
-	directives: [ InfiniteScroll, Scroller, AxisResolver, PositionResolver ]
+	directives: [ InfiniteScroll, AxisResolver, PositionResolver ]
 }
