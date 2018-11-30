17a788f8271e6d79917db509f055b5188c63a011
Oren Farhi <farhioren@gmail.com>
2018-11-29 21:37:37 -0500

    tests..

--- examples/src/app/app.module.ts
+++ examples/src/app/app.module.ts
@@ -2,7 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 
-import { InfiniteScrollModule } from 'ngx-infinite-scroll';
+import { InfiniteScrollModule } from '../../../src/ngx-infinite-scroll';
 
 import { AppComponent } from './app.component';
 import { TestInnerComponent } from './test.inner';
