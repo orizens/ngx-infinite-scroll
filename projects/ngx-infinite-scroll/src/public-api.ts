e5b92848914d3b0c0b68bdb84f5176d58ca40a9c
kwiateusz <kwiateusz@users.noreply.github.com>
2024-05-30 23:07:47 +0200

    feat: make scroll directive standalone (#427)
    * chore: remove empty and unused service
    
    * feat: make scroll directive standalone
    
    resolves #423

--- projects/ngx-infinite-scroll/src/public-api.ts
+++ projects/ngx-infinite-scroll/src/public-api.ts
@@ -2,7 +2,6 @@
  * Public API Surface of ngx-infinite-scroll
  */
 
-export * from './lib/ngx-infinite-scroll.service';
 export { InfiniteScrollDirective } from './lib/ngx-infinite-scroll.directive';
 export { InfiniteScrollModule } from './lib/ngx-infinite-scroll.module';
 export {
